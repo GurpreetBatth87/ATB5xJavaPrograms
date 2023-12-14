1a09a8c712fd6a8e16a0442571f52c4aff5ae757
Gurpreetbatth97 <gurpreetbatth87@gmail.com>
2023-12-14 12:12:54 +0530

    AB5x Java program

---
+++ src/lab001.java
@@ -0,0 +1,7 @@
+package src;
+
+public class lab001 {
+    public static void main(String[] args) {
+        System.out.println("hello batth");
+    }
+}
---
+++ src/lab002.java
@@ -0,0 +1,11 @@
+package src;
+
+public class lab002 {
+    public static void main(String[] args) {
+         byte age=10;
+         age =127;
+        System.out.println(age+1);
+        System.out.println(age);
+
+    }
+}
