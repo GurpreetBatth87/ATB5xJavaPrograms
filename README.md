64877c47771940ee8d304d5fea1996d8ef0be97f
Gurpreetbatth97 <gurpreetbatth87@gmail.com>
2023-12-15 15:29:27 +0530

    15 Dec 2023

---
+++ README.md
@@ -0,0 +1,6 @@
+# Java Program
+
+1. JDK , JVM , JRE
+2. Data Types , Variables
+3. Literals, Keywords 
+4. Identifgit iers, Operators
--- src/Basic_02/lab017.java
+++ src/Basic_02/lab017.java
@@ -4,6 +4,7 @@ public class lab017 {
     public static void main(String[] args) {
         byte a=10;
         byte b= 20;
-        System.out.println(a+b);
+        System.out.println(a+b
+        );
     }
 }
