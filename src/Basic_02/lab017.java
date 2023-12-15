0fc29fc31beee8ed302175ada554eea10532da84
Gurpreetbatth97 <gurpreetbatth87@gmail.com>
2023-12-15 15:32:57 +0530

    Update Java Program

--- src/Basic_02/lab017.java
+++ src/Basic_02/lab017.java
@@ -4,7 +4,6 @@ public class lab017 {
     public static void main(String[] args) {
         byte a=10;
         byte b= 20;
-        System.out.println(a+b
-        );
+        System.out.println(a+b);
     }
 }
