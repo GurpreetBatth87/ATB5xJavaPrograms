9ddd8ef025ac4dc39b2ba9e27fed6a214328b070
Gurpreetbatth97 <gurpreetbatth87@gmail.com>
2024-02-06 17:33:51 +0530

    3rd_FEB_Collection

--- src/collection_Set_3rd_feb_2024/lab201.java
+++ src/collection_Set_3rd_feb_2024/lab201.java
@@ -2,7 +2,14 @@ package src.collection_Set_3rd_feb_2024;
 
 public class lab201 {
     public static void main(String[] args) {
+        //java -- > non generic
 
+temp("pramod");
+temp(123);
+temp(false);
+    }
 
+    public static<T> void temp(T x){
+        System.out.println(x);
     }
 }
